c67c881fff729e99ef9a6c32366706f7864b3b7c
tentoshinz <tentoshinz@gmail.com>
2025-01-06 06:35:05 +0900

    fix10

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,20 +3,19 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 rclpy.init()
-node= Node('talker')
-pub= node.create_publisher(Int16, "countup", 10)
-n= 0
+node = Node("takler")
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
 
 
-def cd():
-        global n
-        msg= Int16()
-        msg.data= n
-        pub.publish(msg)
-        n+=1
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
 
 
 def main():
-    node.create_timer(0.5, cd)
+    node.create_timer(0.5, cb)
     rclpy.spin(node)
-
