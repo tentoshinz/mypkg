80a7273ef1f840b82e4230a03439c663f33818a2
tentoshinz <tentoshinz@gmail.com>
2025-01-07 21:54:26 +0900

    kadai2 first

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,18 +1,26 @@
+# SPDX-FileCopyrightText: 2025 tento
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16, String
 
-
-class Listener(Node):
+class sub_week(Node):
     def __init__(self):
-        super().__init__("listener")
-        self.sub = self.create_subscription(Int16, "countup", self.cb, 10)
+        super().__init__("sub_num")
+        self.subnum = self.create_subscription(Int16, "calc_week", self.cb_num, 10)
+        self.substr = self.create_subscription(String, "calc_week_str", self.cb_str, 10)
     
-    def cb(self, msg):
-        self.get_logger().info("Listen: %d" % msg.data) 
+    def cb_num(self, msg):
+        self.get_logger().info("Listen num: %d" % msg.data) 
+
+    def cb_str(self, msg):
+        self.get_logger().info("Listen str: %s" % msg.data) 
 
 
 def main():
     rclpy.init()
-    node= Listener()
+    node= sub_week()
     rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,26 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
+today=`date "+%Y/%m/%d"`
+
+todayweek=`date "+%w"`
+weekarray=("Sun" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat")
+weekstr=${weekarray[${todayweek}]}
+
+timeout 10 ros2 launch mypkg future_week_calc.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |
-grep 'Listen: 10'
+grep Listen str: ${today} is ${weekstr}
+
+
+{ timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 20040601"; } &
+timeout 10 ros2 run mypkg zellers > /tmp/mypkg.log
+wait
+cat /tmp/mypkg.log |
+grep Listen str: 2
+
+{ timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 19920719"; } &
+timeout 10 ros2 run mypkg zellers > /tmp/mypkg.log
+wait
+cat /tmp/mypkg.log |
+grep Listen str: 0
