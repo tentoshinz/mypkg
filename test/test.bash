2ed30cb9d926708e7458d0819a5ac6eb6a5aa890
tentoshinz <tentoshinz@gmail.com>
2025-01-09 16:57:22 +0900

    fix test

--- test/test.bash
+++ test/test.bash
@@ -37,7 +37,7 @@ ROS_PID=$!
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 20040601"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg1.log; } &
 wait $!
-sleep 2
+sleep 5
 cat /tmp/mypkg1.log
 cat /tmp/mypkg1.log |
 grep "data: 3" || error "$LINENO"
@@ -47,7 +47,7 @@ grep "data: 3" || error "$LINENO"
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 19920719"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg2.log; } &
 wait $!
-sleep 2
+sleep 5
 cat /tmp/mypkg2.log
 cat /tmp/mypkg2.log |
 grep "data: 1" || error "$LINENO"
@@ -56,7 +56,7 @@ grep "data: 1" || error "$LINENO"
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 19780216"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg3.log; } &
 wait $!
-sleep 2
+sleep 5
 cat /tmp/mypkg3.log
 cat /tmp/mypkg3.log |
 grep "data: 5" || error "$LINENO"
@@ -65,7 +65,7 @@ grep "data: 5" || error "$LINENO"
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 20250105"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg4.log; } &
 wait $!
-sleep 2
+sleep 5
 cat /tmp/mypkg4.log
 cat /tmp/mypkg4.log |
 grep "data: 1" || error "$LINENO"
