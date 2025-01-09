9532fd8266944dd65bf8212b6b80cc934a966b0f
tentoshinz <tentoshinz@gmail.com>
2025-01-09 16:47:13 +0900

    fix test

--- test/test.bash
+++ test/test.bash
@@ -24,17 +24,16 @@ weekarray=("Sun" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat")
 weekstr=${weekarray[${todayweek}]}
 
 timeout 10 ros2 launch mypkg future_week_calc.launch.py > /tmp/mypkg.log
-wait $!
 sleep 2
 cat /tmp/mypkg.log
 cat /tmp/mypkg.log |
 grep "Listen str: ${today} is ${weekstr}" || error "$LINENO"
 
 
-# ros2 run mypkg zellers &
-# ROS_PID=$!
+ros2 run mypkg zellers &
+ROS_PID=$!
 
-{ timeout 8 ros2 run mypkg zellers; } &
+# { timeout 8 ros2 run mypkg zellers; } &
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 20040601"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg1.log; } &
 wait $!
@@ -44,7 +43,7 @@ cat /tmp/mypkg1.log |
 grep "data: 3" || error "$LINENO"
 
 
-{ timeout 8 ros2 run mypkg zellers; } &
+# { timeout 8 ros2 run mypkg zellers; } &
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 19920719"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg2.log; } &
 wait $!
@@ -53,7 +52,7 @@ cat /tmp/mypkg2.log
 cat /tmp/mypkg2.log |
 grep "data: 1" || error "$LINENO"
 
-{ timeout 8 ros2 run mypkg zellers; } &
+# { timeout 8 ros2 run mypkg zellers; } &
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 19780216"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg3.log; } &
 wait $!
@@ -62,7 +61,7 @@ cat /tmp/mypkg3.log
 cat /tmp/mypkg3.log |
 grep "data: 5" || error "$LINENO"
 
-{ timeout 8 ros2 run mypkg zellers; } &
+# { timeout 8 ros2 run mypkg zellers; } &
 { timeout 10 ros2 topic pub /date std_msgs/msg/UInt32 "data: 20250105"; } &
 { timeout 12 ros2 topic echo /calc_week > /tmp/mypkg4.log; } &
 wait $!
@@ -71,20 +70,22 @@ cat /tmp/mypkg4.log
 cat /tmp/mypkg4.log |
 grep "data: 1" || error "$LINENO"
 
-# kill -SIGINT $ROS_PID
+kill -SIGINT $ROS_PID
 
 
 sleep 5
+
+echo "res= $res"
 ls /tmp/
-echo "$res"
+# echo 0
 cat /tmp/mypkg.log
-echo 1
+# echo 1
 cat /tmp/mypkg1.log
-echo 2
+# echo 2
 cat /tmp/mypkg2.log
-echo 3
+# echo 3
 cat /tmp/mypkg3.log
-echo 4
+# echo 4
 cat /tmp/mypkg4.log
 
 
